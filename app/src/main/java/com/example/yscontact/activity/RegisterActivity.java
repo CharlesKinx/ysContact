c2c9db1c77617bfd25de578dff3529e1a35b0270
CharlesKinx <CharlesKinx@outlook.com>
2021-06-08 01:00:08 +0800

    完成注册逻辑

--- app/src/main/java/com/example/yscontact/activity/RegisterActivity.java
+++ app/src/main/java/com/example/yscontact/activity/RegisterActivity.java
@@ -1,16 +1,84 @@
 package com.example.yscontact.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.yscontact.R;
+import com.example.yscontact.model.UserInfo;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText userName;
+    private EditText userPhone;
+    private EditText userPs;
+    private EditText userPs1;
+
+    private Button confirm;
+    private Button back;
+
+    private void initView(){
+
+        userName = findViewById(R.id.et_user_name);
+        userPhone = findViewById(R.id.et_phone);
+        userPs = findViewById(R.id.et_psw);
+        userPs1 = findViewById(R.id.et_psw1);
+
+        confirm = findViewById(R.id.btn_ok);
+        back = findViewById(R.id.btn_back);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initView();
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserInfo userInfo = new UserInfo();
+
+                String name = userName.getText().toString();
+                String phone = userPhone.getText().toString();
+                String password = userPs.getText().toString();
+                String password1 = userPs1.getText().toString();
+
+                System.out.println(name);
+
+                if(name.equals("")){
+                    Toast.makeText(RegisterActivity.this,"用户名不能为空！",Toast.LENGTH_SHORT).show();
+                }
+                if(phone.equals("")){
+                    Toast.makeText(RegisterActivity.this,"手机号不能为空！",Toast.LENGTH_SHORT).show();
+                }
+                if(password.equals("")){
+                    Toast.makeText(RegisterActivity.this,"密码不能为空！",Toast.LENGTH_SHORT).show();
+                }
+                if(password1.equals("")){
+                    Toast.makeText(RegisterActivity.this,"密码不能为空！",Toast.LENGTH_SHORT).show();
+                }
+
+                if(!password.equals(password1)){
+                    Toast.makeText(RegisterActivity.this,"两次密码不一致！",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 
 }
---
+++ app/src/main/java/com/example/yscontact/model/UserInfo.java
@@ -0,0 +1,41 @@
+package com.example.yscontact.model;
+
+public class UserInfo {
+    private String userName;
+    private String userPassword;
+    private String userPhone;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userPhone='" + userPhone + '\'' +
+                '}';
+    }
+}
