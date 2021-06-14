6308880f274ef9723839ce3b36209a8afa133b16
CharlesKinx <CharlesKinx@outlook.com>
2021-06-14 20:58:20 +0800

    修改注册时的返回页面

--- app/src/main/java/com/example/yscontact/activity/RegisterActivity.java
+++ app/src/main/java/com/example/yscontact/activity/RegisterActivity.java
@@ -43,8 +43,7 @@ public class RegisterActivity extends AppCompatActivity {
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
 
